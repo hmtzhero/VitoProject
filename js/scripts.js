ac90143f47dfdee947ea6bdab7f2b3bcf4afe9fb
Hector Martinez <hmtzhero@gmail.com>
2016-10-05 23:10:54 -0500

    Inicie los commits esta es la pag original solo con cambios en el index

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){   
+	$(".button-collapse").sideNav();
+	// $('.carousel').carousel({full_width: true});
+	$('.carousel').carousel();
+	// window.setInterval(function(){$('.carousel').carousel('next')},3000);
+	$('.slider').slider({full_width: true});
+
+	$("ul.side-nav > li").on('click',function(){
+		var li = $(this).attr('class');
+		links(li);
+	});
+});
+
+function links(adonde){
+	switch(adonde)
+	{
+		case 'home': 
+			window.location = 'index.html';
+			break;
+		case 'about': 
+			window.location = 'about.html';
+			break;
+		case 'services':
+			window.location = 'services.html';
+			break;
+		case 'faq':
+			window.location = 'faq.html';
+			break;
+		case 'contact':
+			window.location = 'contact.html';
+		default:
+	}	
+}
+
+function iniciar() {
+	place = new google.maps.LatLng(25.645452, -100.323105)
+	var mapOptions = {
+		center: place,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+	var marker = new google.maps.Marker({
+        position: place, 
+        title: 'VITO CONTRACTORS & TRANSPORTATION,S.A. DE C.V.', 
+        map: map, 
+    });
+} 
